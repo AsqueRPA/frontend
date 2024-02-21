afac186ff6091a65b51aac6c46071909b708fb2b
Hugo Zhan <hugozhan0802@gmail.com>
2024-02-21 15:54:39 -0800

    added number of responses field

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-// App.js
-import React, { useState } from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import logo from './capylogo.png';
-import MainContent from './MainContent'; // Import the new component
-import { Box } from '@mui/material';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import HowItWorks from './HowItWorks'; // Import the new component
-import Button from '@mui/material/Button';
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import logo from "./capylogo.png";
+import MainContent from "./MainContent";
+import { Box } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HowItWorks from "./HowItWorks";
+import Button from "@mui/material/Button";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
   },
 });
 
@@ -22,58 +21,66 @@ export function Footer() {
   return (
     <Box sx={styles.footer}>
       <Typography variant="body2">
-        Email us at <a href="dyllan@berkeley.edu" style={{ color: 'inherit', textDecoration: 'none' }}>dyllan@berkeley.edu</a>
+        Email us at{" "}
+        <a
+          href="dyllan@berkeley.edu"
+          style={{ color: "inherit", textDecoration: "none" }}
+        >
+          dyllan@berkeley.edu
+        </a>
       </Typography>
-      <Typography variant="body2">
-        © Copyright 2024 LineUpp, Corp.
-      </Typography>
+      <Typography variant="body2">© Copyright 2024 LineUpp, Corp.</Typography>
     </Box>
-  )
+  );
 }
 
 function App() {
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const { email, targetAudience, question } = event.target.elements; // Destructure the form elements
-
-    const formData = {
-      email: email.value,
-      targetAudience: targetAudience.value,
-      question: question.value,
-    };
-
-    // Replace 'YOUR_ZAPIER_WEBHOOK_URL' with the URL provided by Zapier
-    await fetch('https://hooks.zapier.com/hooks/catch/17339037/3l7tlks/', {
-      method: 'POST',
-      body: JSON.stringify(formData),
-    }).catch(error => console.error('Fetch error:', error));
-    setIsSubmitted(true);
-    console.log("Form submitted");
-  };
-
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Router>
-        <AppBar position="static" color="default" elevation={0} sx={styles.appBar}>
+        <AppBar
+          position="static"
+          color="default"
+          elevation={0}
+          sx={styles.appBar}
+        >
           <Toolbar>
-            <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-              <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-                <img src={logo} alt="Logo" style={{ padding: '1rem 5%', maxHeight: '150px', cursor: 'pointer' }} />
+            <Typography
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+                <img
+                  src={logo}
+                  alt="Logo"
+                  style={{
+                    padding: "1rem 5%",
+                    maxHeight: "150px",
+                    cursor: "pointer",
+                  }}
+                />
               </Link>
             </Typography>
-            <Button variant="outlined" color="primary" component={Link} to="/howitworks" style={{ textDecoration: 'none', marginRight: '100px' }}>
+            <Button
+              variant="outlined"
+              color="primary"
+              component={Link}
+              to="/howitworks"
+              style={{ textDecoration: "none", marginRight: "100px" }}
+            >
               How It Works
             </Button>
           </Toolbar>
         </AppBar>
         <Routes>
           <Route path="/howitworks" element={<HowItWorks />} />
-          <Route path="/" element={<MainContent isSubmitted={isSubmitted} handleSubmit={handleSubmit} />} />
+          <Route path="/" element={<MainContent />} />
         </Routes>
       </Router>
+      <Footer />
     </ThemeProvider>
   );
 }
@@ -83,56 +90,56 @@ export const styles = {
     borderBottom: `1px solid ${darkTheme.palette.divider}`,
   },
   mainContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: 'calc(100vh - 64px)',
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "calc(100vh - 64px)",
   },
   mainContent: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '2rem 5%',
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "2rem 5%",
     flexGrow: 1,
   },
   thankYouContent: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     flexGrow: 1,
   },
   textContent: {
-    maxWidth: '50%',
+    maxWidth: "50%",
   },
   card: {
     minWidth: 600,
-    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-    padding: '20px',
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+    padding: "20px",
   },
   container: {
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
     gap: 2,
   },
   textField: {
-    width: '100%',
+    width: "100%",
   },
   submitButton: {
     marginTop: 2,
-    width: '100%',
+    width: "100%",
   },
   thankYouBox: {
-    maxWidth: '600px',
-    padding: '20px',
-    textAlign: 'center',
-    backgroundColor: 'background.paper',
-    borderRadius: '10px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+    maxWidth: "600px",
+    padding: "20px",
+    textAlign: "center",
+    backgroundColor: "background.paper",
+    borderRadius: "10px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
   },
   footer: {
-    textAlign: 'center',
-    padding: '20px',
-    marginTop: 'auto',
-    backgroundColor: 'background.paper',
+    textAlign: "center",
+    padding: "20px",
+    marginTop: "auto",
+    backgroundColor: "background.paper",
     borderTop: `1px solid ${darkTheme.palette.divider}`,
   },
 };
--- src/MainContent.js
+++ src/MainContent.js
@@ -1,57 +1,292 @@
-// MainContent.js
-import React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { Footer, styles } from './App'; 
-import { Grid } from '@mui/material';
+import React, { useState } from "react";
+import {
+  ThemeProvider,
+  createTheme,
+  Box,
+  TextField,
+  Button,
+  Card,
+  CardContent,
+  CssBaseline,
+  Typography,
+  AppBar,
+  Toolbar,
+  Alert,
+} from "@mui/material";
+import logo from "./capylogo.png";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
-function MainContent({ isSubmitted, handleSubmit }) {
+function MainContent() {
+  const baseUrl = process.env.REACT_APP_BASE_URL;
+
+  const [email, setEmail] = useState("");
+  const [targetAudience, setTargetAudience] = useState("");
+  const [numberOfResponses, setNumberOfResponses] = useState("");
+  const [question, setQuestion] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [severity, setSeverity] = useState("");
+  const [errors, setErrors] = useState({
+    email: false,
+    targetAudience: false,
+    numberOfResponses: false,
+    question: false,
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newErrors = {
+      email: email === "",
+      targetAudience: targetAudience === "",
+      numberOfResponses: numberOfResponses === "",
+      question: question === "",
+    };
+    setErrors(newErrors);
+
+    const isFormValid = !Object.values(newErrors).includes(true);
+
+    if (isFormValid) {
+      const data = {
+        email,
+        keyword: targetAudience,
+        targetAmountResponse: numberOfResponses,
+        question,
+      };
+
+      fetch(`${baseUrl}/request-reachout`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => {
+          if (res.status === 200) {
+            setSeverity("success");
+            setEmail("");
+            setTargetAudience("");
+            setNumberOfResponses("");
+            setQuestion("");
+            setErrors({
+              email: false,
+              targetAudience: false,
+              numberOfResponses: false,
+              question: false,
+            });
+          } else {
+            setSeverity("error");
+          }
+          setShowAlert(true);
+          setTimeout(() => {
+            setShowAlert(false);
+          }, 5000);
+          // const formData = {
+          //   email,
+          //   targetAudience,
+          //   question,
+          // };
+
+          // // Replace 'YOUR_ZAPIER_WEBHOOK_URL' with the URL provided by Zapier
+          // await fetch('https://hooks.zapier.com/hooks/catch/17339037/3l7tlks/', {
+          //   method: 'POST',
+          //   body: JSON.stringify(formData),
+          // }).catch(error => console.error('Fetch error:', error));
+        })
+        .catch(() => {
+          setSeverity("error");
+          setShowAlert(true);
+          setTimeout(() => {
+            setShowAlert(false);
+          }, 5000);
+        });
+    } else {
+      setSeverity("error");
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 5000);
+    }
+  };
+
   return (
-    <Box sx={styles.mainContainer}>
-      {isSubmitted ? (
-        <Box sx={styles.thankYouContent}>
-          <Box sx={styles.thankYouBox}>
-            <Typography variant="h4" component="h1" gutterBottom>
-              <b>We Have Received Your Request</b>
-            </Typography>
-            <Typography variant="h6" component="p">
-              We have processed your request and are currently gathering responses based on your criteria.
-              Please check email in the next few days for a Google Sheet containing all the responses.
-              Should you have any questions or feedbacks, please feel free to contact us at dyllan@berkeley.edu.
-            </Typography>
-          </Box>
-        </Box>
-      ) : (
-        <Box sx={styles.mainContent}>
-          <Box sx={styles.textContent}>
-            <Typography variant="h3" gutterBottom>
-              SurveyBara | Survey ICPs at scale
-            </Typography>
-            <Typography variant="h6">
-              (1) Define your target audience. <br /> (2) Ask your question. <br /> (3) Get a google sheet of responses mapped to LinkedIn Profiles in days.
-            </Typography>
-          </Box>
-          <Card sx={styles.card}>
-            <CardContent>
-              <Box component="form" sx={styles.container} onSubmit={handleSubmit} noValidate autoComplete="off">
-                <TextField name="email" label="Your Email" variant="outlined" sx={styles.textField} />
-                <TextField name="targetAudience" label="Target Audience" variant="outlined" sx={styles.textField} />
-                <TextField name="question" label="Question" multiline rows={4} defaultValue="Ask a question you can imagine yourself replying to" sx={styles.textField} />
-
-                <Button type="submit" variant="outlined" color="primary" sx={styles.submitButton}>Submit</Button>
-              </Box>
-            </CardContent>
-          </Card>
-        </Box>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      {showAlert && (
+        <Alert
+          severity={severity}
+          onClose={() => {
+            setShowAlert(false);
+          }}
+          style={{
+            position: "fixed",
+            top: "2rem",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          {severity === "success"
+            ? "Your request has been submitted successfully. We will get back to you soon."
+            : "There was an error submitting your request. Please try again."}
+        </Alert>
       )}
-      <Footer />
-    </Box>
+      <Box sx={styles.mainContent}>
+        <Box sx={styles.textContent}>
+          <Typography variant="h3" gutterBottom>
+            SurveyBara | Survey ICPs at scale
+          </Typography>
+          <Typography variant="h6">
+            (1) Define your target audience. <br /> (2) Ask your question.{" "}
+            <br /> (3) Get a google sheet of replies mapped to LinkedIn Profiles
+            in days.
+          </Typography>
+        </Box>
+        <Card sx={styles.card}>
+          <CardContent>
+            <Box
+              component="form"
+              sx={styles.container}
+              onSubmit={handleSubmit}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="email"
+                label="Your Email"
+                variant="outlined"
+                required
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                onFocus={(e) => {
+                  setErrors({ ...errors, email: false });
+                }}
+                onBlur={(e) => {
+                  setErrors({ ...errors, email: e.target.value === "" });
+                }}
+                sx={styles.textField}
+                error={errors.email}
+              />
+              <TextField
+                id="target-audience"
+                label="Target Audience"
+                variant="outlined"
+                required
+                value={targetAudience}
+                onChange={(e) => {
+                  setTargetAudience(e.target.value);
+                }}
+                onFocus={(e) => {
+                  setErrors({ ...errors, targetAudience: false });
+                }}
+                onBlur={(e) => {
+                  setErrors({
+                    ...errors,
+                    targetAudience: e.target.value === "",
+                  });
+                }}
+                sx={styles.textField}
+                error={errors.targetAudience}
+              />
+              <TextField
+                id="number-of-responses"
+                type="number"
+                inputProps={{ min: 0, max: 100 }}
+                onChange={(e) => {
+                  if (e.target.value === "") {
+                    setNumberOfResponses("");
+                    return;
+                  }
+                  const value = Math.min(Math.max(0, e.target.value), 100);
+                  setNumberOfResponses(value);
+                }}
+                onFocus={(e) => {
+                  setErrors({ ...errors, numberOfResponses: false });
+                }}
+                onBlur={(e) => {
+                  setErrors({
+                    ...errors,
+                    numberOfResponses: e.target.value === "",
+                  });
+                }}
+                label="Number of Responses"
+                variant="outlined"
+                required
+                value={numberOfResponses}
+                sx={styles.textField}
+                error={errors.numberOfResponses}
+              />
+              <TextField
+                id="outlined-multiline-static"
+                label="Question"
+                required
+                multiline
+                rows={4}
+                value={question}
+                onChange={(e) => {
+                  setQuestion(e.target.value);
+                }}
+                onFocus={(e) => {
+                  setErrors({ ...errors, question: false });
+                }}
+                onBlur={(e) => {
+                  setErrors({ ...errors, question: e.target.value === "" });
+                }}
+                sx={styles.textField}
+                error={errors.question}
+              />
+              <Button
+                type="submit"
+                variant="outlined"
+                color="primary"
+                sx={styles.submitButton}
+              >
+                Submit
+              </Button>
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
+    </ThemeProvider>
   );
 }
+
+const styles = {
+  appBar: {
+    borderBottom: `1px solid ${darkTheme.palette.divider}`,
+  },
+  mainContent: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    minHeight: "calc(100vh - 64px)",
+    padding: "2rem 5%",
+    marginTop: "-64px",
+  },
+  textContent: {
+    maxWidth: "50%",
+  },
+  card: {
+    minWidth: 600,
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+    padding: "20px",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 2,
+  },
+  textField: {
+    width: "100%",
+  },
+  submitButton: {
+    marginTop: 2,
+    width: "100%",
+  },
+};
 
 export default MainContent;
